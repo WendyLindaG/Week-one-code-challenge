3fd6564b5f426a423419f90d0a59ed723b57377e
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-22 13:17:18 +0300

    completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Week-one-code-challenge
+
+A tool called Student Grade Generator invites the user to enter their students' grades and then generates the appropriate grade based on those marks.
+A program called "Speed Detector" uses a car's speed as input and penalizes the driver with one demerit point for every five kilometers per hour beyond the posted limit (70). "License suspended" is reported by the computer if the driver receives more than 12 points.
+Net salary program determines an employee's net compensation based on their base pay and benefits and Payee Tax, NHIF, and NSSF deductions.
---
+++ gradeGen.js
@@ -0,0 +1,21 @@
+const grader = document.querySelector('button')
+grader.addEventListener('click', function(){
+    const marksInput = document.querySelector('#marks');
+    const marks = parseInt(marksInput.value);
+    let grade = '';
+    if (marks >= 79 && marks <= 100) {
+        grade = "A";
+      } else if (marks >= 60 && marks <= 79) {
+        grade = "B";
+      } else if (marks >= 50 && marks <= 59) {
+        grade = "C";
+      } else if (marks >= 40 && marks <= 49) {
+        grade = "D";
+      } else if (marks>=0 && marks <= 39 ){
+        grade = "E";
+      } else {
+        grade = "INVALID INPUT"
+      }
+        alert(`Grade: ${grade}`);
+})
+
